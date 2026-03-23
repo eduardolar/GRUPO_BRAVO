b3733e3ed6191ac84c5881814aeda41fb605e68a
Jose Marin Carrillo <josemc27.2.2000@gmail.com>
2026-03-23 12:32:14 +0100

    estilos de colores en core

---
+++ frontend/lib/core/colors_style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  //colores base
+  static const Color background = Color(0xFF000000);
+  static const Color panel = Color(0xFF1A1A1A);
+  static const Color line = Color(0xFF2A2A2A);
+
+  //Detalles de la APP
+  static const Color button = Color(0xFFD4AF37);
+  static const Color sombra = Color(0xFFB8860B);
+
+   //Texto
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecundary = Color(0xFFEEEEEE);
+
+  //Iconos
+  static const Color iconPrimary = Color(0xFFFFFFFF);
+  static const Color iconDetail = Color(0xFFD4AF37);
+
+  //Alertas o notificaciones
+  static const Color error = Color(0xFFFF3B30);
+
+  //Sombras
+  static const Color shadow = Color(0x66000000);
+
+}
