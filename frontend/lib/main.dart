8da03add07e288849abd967dd71871ed279f2050
Eduardo Lara <edularaqwe@gmail.com>
2026-04-17 13:18:33 +0200

    Cambio de diselo en Confirmar Pedido

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -30,7 +30,7 @@ class MainApp extends StatelessWidget {
       ],
       child: MaterialApp(
         theme: ThemeData(textTheme: GoogleFonts.frederickaTheGreatTextTheme()),
-        home: const HomeTrabajador(),
+        home: const HomeScreen(),
       ),
     );
   }
